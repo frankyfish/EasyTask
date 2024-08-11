a8b65d6930592c33030693dd32ff7e816b54ac14
frankyfish <fr.fish93@gmail.com>
2024-08-11 14:06:02 +0200

    new component; 40,

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,24 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'first-app';
   users = DUMMY_USERS
+
+  selectedUser!:string
+  
+  onUserSelected(username: string) {
+    console.log('user selected:' + username)
+    this.selectedUser = username
+  }
 }
+
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomUserId = Math.floor(Math.random() * DUMMY_USERS.length)
+
+type User = {
+  id: string,
+  avatar: string,
+  name: string
+}
 
 @Component({
   selector: 'app-user',
@@ -11,12 +17,19 @@ const randomUserId = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input() avatar!: string
-  @Input() name!: string
+  @Input({required: true}) user!: User
+  // @Input({required: true}) id!: string
+  // @Input({required: true}) avatar!: string
+  // @Input({required: true}) name!: string
+  @Output() select = new EventEmitter<string>()
+  @Output() username = new EventEmitter<string>()
 
   get avatarPath() {
-    return '/assets/users/' + this.avatar
+    return '/assets/users/' + this.user.avatar
   }
 
-  onUserClicked() {}
+  onUserClicked() {
+    this.select.emit(this.user.id)
+    this.username.emit(this.user.name)
+  }
 }
