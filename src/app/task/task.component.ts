9ef0b562539f9621fc2c7fec683fb830aced4961
frankyfish <fr.fish93@gmail.com>
2024-08-15 22:26:27 +0200

    (57) app finished feature-wise

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,11 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { type Task } from './task.model';
+import { CardComponent } from "../shared/card/card.component";
+import { DatePipe } from '@angular/common';
 export { Task }; // todo: what is this (auto-gen)
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
